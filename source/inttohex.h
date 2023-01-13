1737099fb3df4c2052e4d78a7343536aad241516
natlud-snhu <120280806+natlud-snhu@users.noreply.github.com>
2023-01-13 10:53:31 -0600

    Delete source directory

--- source/inttohex.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <sstream>
-#include <string>
-
-std::string intToHex(unsigned int num); // converts an integer to a hex string
--- source/validation.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-
-bool isPasswordValid(std::string pass); //returns a boolean if the password is valid
-bool isInputValid(std::string input); //returns a boolean if the input is valid
